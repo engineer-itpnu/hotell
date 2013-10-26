9bc70df7dd5bcf81395cf5e3ee543d8efa63dc81
msajadi824 <msajadi824@gmail.com>
2013-10-26 23:03:40 +0330

    remove acme demo bundle fix index page

--- src/Hotel/reserveBundle/Controller/DefaultController.php
+++ src/Hotel/reserveBundle/Controller/DefaultController.php
@@ -6,16 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('HotelreserveBundle:Default:index.html.twig', array('name' => $name));
-    }
     public function mainAction()
     {
-        return $this->render('HotelreserveBundle::main.html.twig');
-    }
-    public function loginAction()
-    {
-        return $this->render('HotelreserveBundle::login.html.twig');
+        return $this->render('HotelreserveBundle::index.html.twig');
     }
 }
