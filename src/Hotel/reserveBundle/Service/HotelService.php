55f849c26f7261324af30dcae395ef4640d69ada
msajadi824 <msajadi824@gmail.com>
2013-11-24 13:35:34 +0330

    fix service

--- src/Hotel/reserveBundle/Service/HotelService.php
+++ src/Hotel/reserveBundle/Service/HotelService.php
@@ -35,40 +35,62 @@ class HotelService {
         $this->encoderFactory = $ef;
     }
 
+    //-----------------------------------------------------------------------------
     public function ListRooms($input)
     {
         $RoomListRequest = new RoomListRequest();
         HotelService::CopyObject($input,$RoomListRequest);
 
+        if(!$this->checkUser($RoomListRequest->agency_info))
+            return new RoomListResponse("User Not Verified",null);
+
         HotelService::log("city:".$RoomListRequest->city);
         HotelService::log("date:".$RoomListRequest->date);
         HotelService::log("days:".$RoomListRequest->days_count);
-        return 12;
+        HotelService::log("Username:".$RoomListRequest->agency_info->username);
+        HotelService::log("Password:".$RoomListRequest->agency_info->password);
+
+        return new RoomListResponse("Success",null);
     }
 
+    //-----------------------------------------------------------------------------
     public function PreReserve($input)
     {
-        $username = $input->Userinfo->UserName;
-        $password = $input->Userinfo->Password;
-        $roomid = $input->roomid;
+        $PreReserveRequest = new PreReserveRequest();
+        HotelService::CopyObject($input,$PreReserveRequest);
 
-        return "user: $username pass: $password roomid: $roomid ";
+        if(!$this->checkUser($PreReserveRequest->agency_info))
+            return new PreReserveResponse(null,null,null,"User Not Verified");
+
+        HotelService::log("hotel:".$PreReserveRequest->hotel_code);
+        HotelService::log("room:".$PreReserveRequest->room_code);
+        HotelService::log("date:".$PreReserveRequest->date);
+        HotelService::log("days:".$PreReserveRequest->days_count);
+        HotelService::log("extra capacity:".$PreReserveRequest->extra_capacity_count);
+        HotelService::log("customer:".$PreReserveRequest->customer_code);
+        HotelService::log("Username:".$PreReserveRequest->agency_info->username);
+        HotelService::log("Password:".$PreReserveRequest->agency_info->password);
+
+        return new PreReserveResponse(null,null,null,"Success");
     }
 
+    //-----------------------------------------------------------------------------
     public function Reserve($input)
     {
         $ReserveRequest = new ReserveRequest();
         HotelService::CopyObject($input,$ReserveRequest);
 
         if(!$this->checkUser($ReserveRequest->agency_info))
-            return new ReserveResponse(null,null,"User Not Verify");
+            return new ReserveResponse(null,null,"User Not Verified");
 
         HotelService::log("Reserve_code:".$ReserveRequest->reserve_code);
         HotelService::log("Username:".$ReserveRequest->agency_info->username);
         HotelService::log("Password:".$ReserveRequest->agency_info->password);
-        return new ReserveResponse(10,1000,"True");
+
+        return new ReserveResponse(null,null,"Success");
     }
 
+    //-----------------------------------------------------------------------------
     private function checkUser($agency)
     {
         $user = $this->em->getRepository("HotelreserveBundle:userEntity")->findOneBy(array("username"=>$agency->username));
