9f456f8e7d5fe0f814f9225b088745650501cc37
croner <engineer.itpnu@yahoo.com>
2013-12-10 12:44:35 +0330

    fix Edit Reporting

--- src/Hotel/reserveBundle/Form/ReportingReserveType.php
+++ src/Hotel/reserveBundle/Form/ReportingReserveType.php
@@ -17,10 +17,16 @@ class ReportingReserveType extends AbstractType
         $builder
             ->add('RfromDateTime','shamsi_date',array('required'=>false))
             ->add('RtoDateTime','shamsi_date',array('required'=>false))
-            ->add('customerEntity','entity', array('class' => 'HotelreserveBundle:customerEntity',
-                'required'=>false,'empty_value' => 'همه مسافران',))
+            ->add('cust_name','entity', array('class' => 'HotelreserveBundle:customerEntity',
+                'property' => 'cust_name','required'=>false,'empty_value' => 'نام مسافر',))
+            ->add('cust_family','entity', array('class' => 'HotelreserveBundle:customerEntity',
+                'property' => 'cust_family','required'=>false,'empty_value' => 'نام خانوادگی مسافر',))
+            ->add('cust_mobile','entity', array('class' => 'HotelreserveBundle:customerEntity',
+                'property' => 'cust_mobile','required'=>false,'empty_value' => 'شماره همراه مسافر',))
             ->add('RagencyEntity','entity', array('class' => 'HotelreserveBundle:agencyEntity',
                 'property' => 'agency_name','required'=>false,'empty_value' => 'همه آژانس ها',))
+            ->add('hotel_city','entity', array('class' => 'HotelreserveBundle:hotelEntity',
+                'property' => 'hotel_city','required'=>false,'empty_value' => 'شهر اقامت',))
             ->add('RhotelEntity','entity', array('class' => 'HotelreserveBundle:hotelEntity',
                 'property' => 'hotel_name','required'=>false,'empty_value' => 'همه هتل ها ',))
         ;
