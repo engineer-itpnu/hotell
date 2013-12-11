117e6c5ff22c5880a9277751b1a004f3867c52dc
croner <engineer.itpnu@yahoo.com>
2013-12-11 12:33:45 +0330

    fix Edit Reporting

--- src/Hotel/reserveBundle/Form/reportingType.php
+++ src/Hotel/reserveBundle/Form/reportingType.php
@@ -20,10 +20,14 @@ class reportingType extends AbstractType
             ))
             ->add('fromDateTime','shamsi_date',array('required'=>false))
             ->add('toDateTime','shamsi_date',array('required'=>false))
-            ->add('hotelEntity','entity', array('class' => 'HotelreserveBundle:hotelEntity',
-                'property' => 'hotel_name','required'=>false,'empty_value' => 'همه هتل ها ',))
             ->add('agencyEntity','entity', array('class' => 'HotelreserveBundle:agencyEntity',
                 'property' => 'agency_name','required'=>false,'empty_value' => 'همه آژانس ها',))
+//            ->add('hotel_city','entity', array('class' => 'HotelreserveBundle:hotelEntity',
+//                'property' => 'hotel_city','required'=>false,'empty_value' => 'همه شهرها',
+//                'query_builder' => function (EntityRepository $er) {
+//                    return $er->createQueryBuilder('u')->groupBy("u.hotel_city");
+//                }))
+            ->add('hotelEntity','choice', array('required'=>false,'choices' => array(''=>'همه هتل ها ')))
         ;
     }
 
